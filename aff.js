d412d47ca48cf3f86bf8985a1d5e1381b4065671
Oi '- <tosoltandofds@gmail.com>
2017-02-16 09:43:36 -0300

    affs
    odiei isso '='

---
+++ aff.js
@@ -0,0 +1,44 @@
+coisas = {
+	"rj" : {
+		titulo: "Rio De Janeiro",
+		desc: "Rio de Janeiro,é um município brasileiro, capital do estado homônimo, situado no Sudeste do país.",
+		clima: "O clima do Rio de Janeiro é o tropical atlântico, com variações locais, devido às diferenças de altitude, vegetação e proximidade do oceano.",
+		pturistico: "Cristo Redentor,Pão de Açúcar,Copacabana,Lagoa Rodrigo de Freitas...",
+		cultura: "O Rio de Janeiro herdou de seu passado uma forte vocação cultural.  Atualmente, o Rio aglutina os principais centros de produção da TV brasileira: o Projac da Rede Globo, o RecNov da Rede Record e o (Polo de Cinema de Jacarepaguá)."
+	}
+		"curitiba" : {
+		titulo: "Curitiba",
+		desc: "Curitiba é um município brasileiro, capital do estado do Paraná. É o município mais populoso do Paraná e da região Sul, além de ser o 8º mais populoso do país.",
+		clima: "Curitiba tem um clima temperado.",
+		pturistico: "Ópera de Arame,Jardim Botânico,Museu Oscar Niemeyer...",
+		cultura: "O Festival de Teatro de Curitiba, um dos mais importantes festivais de teatro do país, ocorre desde 1992, habitualmente composto de atrações internacionais, grande atrações nacionais, montagens locais e uma mostra alternativa.Em Curitiba há alguns festivais anuais. Alguns deles são diretamente dedicados às artes, como o Festival de Teatro de Curitiba e a Oficina de Música de Curitiba. A cidade de Curitiba conta também com a Bienal Internacional de Curitiba."
+	}
+		"sp" : {
+		titulo: "São Paulo",
+		desc: "São Paulo é o principal centro financeiro, corporativo e mercantil da América do Sul.É a cidade mais populosa do Brasil, do continente americano, da lusofonia e de todo o hemisfério sul.",
+		clima: "O clima de São Paulo é considerado subtropical úmido.",
+		pturistico: "Parque Ibirapuera,Museu de Arte de São Paulo,Theatro Municipal...",
+		cultura: "São Paulo possui uma ampla rede de teatros, casas de show e espetáculo, bares e grandes eventos culturais como a Bienal de São Paulo e a Virada Cultural."
+	}
+			"bahia" : {
+		titulo: "Bahia",
+		desc: "A Bahia é uma das 27 unidades federativas do Brasil. Está situada ao sul da Região Nordeste, fazendo limite com outros oito estados brasileiros.",
+		clima: "Devido à sua latitude, o clima tropical predomina em toda a Bahia, apresentando temperaturas elevadas.",
+		pturistico: "Salvador,Ilhéus,Itacaré,Porto Seguro...",
+		cultura: "Nas últimas décadas, a Bahia tem sido um verdadeiro celeiro musical. O Carnaval de Salvador, considerado o maior carnaval de rua do mundo, atrai anualmente 2 milhões de foliões em seis dias de festa."
+	}
+			"amazonas" : {
+		titulo: "Amazonas",
+		desc: "Amazonas é uma das 27 unidades federativas do Brasil, sendo a maior delas em território, com uma área de 1 559 159,148 km², constituindo-se na nona maior subdivisão mundial. Seria o décimo sexto<br> maior país do mundo em área territorial.",
+		clima: "O Amazonas é dominado pelo clima equatorial.",
+		pturistico: "Novo Airão, Barcelos, Parque Nacional de Anavilhanas, Itaocatiara...",
+		cultura: "A cultura amazonense tem seu referencial em três raízes étnicas bem distintas entre si, sendo essencialmente de base ameríndia e europeia. Há registros de diversos festivais nos municípios do estado.<br> Muitos dos festivais mostram a influência do folclore da região. O Carnaval de Manaus é realizado todos os anos na cidade, entre os principais eventos estão o desfile de escolas de samba, o tradicional desfile de fantasias (que acontece no Teatro Amazonas, próximo ao Centro Histórico de Manaus), além do Carnaboi."
+	}
+			"minas" : {
+		titulo: "Minas Gerais",
+		desc: "Minas Gerais é uma das 27 unidades federativas do Brasil, sendo o quarto estado com a maior área territorial e o segundo em quantidade de habitantes, localizada na Região Sudeste do país",
+		clima: "No estado de Minas Gerais, predominam quatro tipos distintos de clima: o clima subtropical de altitude, o clima subtropical de inverno seco, clima tropical com inverno seco e e o clima seco com chuvas no verão.",
+		pturistico: "Mineirão, Lagoa da Pumpulha, Igreja de São Francisco de Assis, Parque do Sabiá...",
+		cultura: "Minas Gerais conta com vários espaços, projetos e eventos dedicados ao fomento das áreas teatral e de dança. O estado é berço de uma considerável gama de grupos teatrais de sucesso nacional ou mesmo internacional."
+	}
+}
---
+++ aff_internacionais.js
@@ -0,0 +1,39 @@
+coisas = {
+		"paris" : {
+		titulo: "Paris",
+		desc: "Paris é a capital mais populosa cidade da França.",
+		clima: "De tipo oceânico de transição",
+		pturistico: "Torre Eiffel,Arco do Triunfo,Museu do Louvre,Panteão...",
+		cultura: "Paris tem um lugar privilegiado no âmbito artístico e cultural a nível mundial nos últimos séculos.<br> Nasceram na cidade movimentos artísticos como o expressionismo, o surrealismo e o fauvismo e importantes figuras da arte e pensamento como René Descartes, Voltaire, Victor Hugo, Émile Zola..."
+	}
+		"dubai" : {
+		titulo: "Dubai",
+		desc: "Dubai é a maior cidade e emirado de mesmo nome dos Emirados Árabes Unidos.",
+		clima: "Predominantemente quente e árido",
+		pturistico: "Palm Islands,The World,Burj Al Arab,Burj Khalifa...",
+		cultura: "Dubai é famosa por suas obras grandiosas e de forte apelo turístico. Dentre elas, podemos destacar as Palm Islands, o arquipélago The World, o hotel Burj Al Arab e o edifício Burj Khalifa."
+	}
+			"newyork" : {
+		titulo: "New York",
+		desc: "New York é um estado no nordeste dos Estados Unidos e é o 27º mais extenso.",
+		clima: "Clima continental úmido",
+		pturistico: "Central Park,Estátua da Liberdade,Empire State Building,Times Square...",
+		cultura: "É uma cidade altamente ativa. Possui muitos cognomes: O mais famoso deles é (A Grande Maçã)."
+	}
+			"puntacana" : {
+		titulo: "Punta Cana",
+		desc: "Punta Cana faz parte do distrito municipal de Punta Cana-Bávaro-Verón-Macao,em La Altagracia,a província mais oriental <br>da República Dominicana.",
+		clima: "",
+		pturistico: "",
+		cultura: ""
+	}
+			"" : {
+		titulo: "",
+		desc: "",
+		clima: "",
+		pturistico: "",
+		cultura: ""
+	}
+	
+	
+}
