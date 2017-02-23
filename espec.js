15e0b52b0be412b77431b42da1edf1023cae2fcd
Oi '- <tosoltandofds@gmail.com>
2017-02-23 10:32:42 -0300

    FIMMMMMMMM
    FINAAAAAAAAAAAAAAAAAL FLASHHH

---
+++ espec.js
@@ -0,0 +1,141 @@
+espec = {
+	   "rj" : {
+		imagem:	"Imagens/rio.jpg",
+		titulo: "Rio De Janeiro",
+		desc: "Rio de Janeiro,é um município brasileiro, capital do estado homônimo, situado no Sudeste do país.",
+		clima: "O clima do Rio de Janeiro é o tropical atlântico, com variações locais, devido às diferenças de altitude, vegetação e proximidade do oceano.",
+		pturistico: "Cristo Redentor,Pão de Açúcar,Copacabana,Lagoa Rodrigo de Freitas...",
+		cultura: "O Rio de Janeiro herdou de seu passado uma forte vocação cultural.  Atualmente, o Rio aglutina os principais centros de produção da TV brasileira: o Projac da Rede Globo, o RecNov da Rede Record e o (Polo de Cinema de Jacarepaguá)."
+	},
+		"curitiba" : {
+			imagem:	"Imagens/curitiba.jpg",
+		titulo: "Curitiba",
+		desc: "Curitiba é um município brasileiro, capital do estado do Paraná. É o município mais populoso do Paraná e da região Sul, além de ser o 8º mais populoso do país.",
+		clima: "Curitiba tem um clima temperado.",
+		pturistico: "Ópera de Arame,Jardim Botânico,Museu Oscar Niemeyer...",
+		cultura: "O Festival de Teatro de Curitiba, um dos mais importantes festivais de teatro do país, ocorre desde 1992, habitualmente composto de atrações internacionais, grande atrações nacionais, montagens locais e uma mostra alternativa.Em Curitiba há alguns festivais anuais. Alguns deles são diretamente dedicados às artes, como o Festival de Teatro de Curitiba e a Oficina de Música de Curitiba. A cidade de Curitiba conta também com a Bienal Internacional de Curitiba."
+	},
+		"sp" : {
+		imagem:	"Imagens/sp.jpg",
+		titulo: "São Paulo",
+		desc: "São Paulo é o principal centro financeiro, corporativo e mercantil da América do Sul.É a cidade mais populosa do Brasil, do continente americano, da lusofonia e de todo o hemisfério sul.",
+		clima: "O clima de São Paulo é considerado subtropical úmido.",
+		pturistico: "Parque Ibirapuera,Museu de Arte de São Paulo,Theatro Municipal...",
+		cultura: "São Paulo possui uma ampla rede de teatros, casas de show e espetáculo, bares e grandes eventos culturais como a Bienal de São Paulo e a Virada Cultural."
+	},
+			"bahia" : {
+		imagem:	"Imagens/bahia.jpg",
+		titulo: "Bahia",
+		desc: "A Bahia é uma das 27 unidades federativas do Brasil. Está situada ao sul da Região Nordeste, fazendo limite com outros oito estados brasileiros.",
+		clima: "Devido à sua latitude, o clima tropical predomina em toda a Bahia, apresentando temperaturas elevadas.",
+		pturistico: "Salvador,Ilhéus,Itacaré,Porto Seguro...",
+		cultura: "Nas últimas décadas, a Bahia tem sido um verdadeiro celeiro musical. O Carnaval de Salvador, considerado o maior carnaval de rua do mundo, atrai anualmente 2 milhões de foliões em seis dias de festa."
+	},
+			"amazonas" : {
+		imagem:	"Imagens/amazonas.jpg",
+		titulo: "Amazonas",
+		desc: "Amazonas é uma das 27 unidades federativas do Brasil, sendo a maior delas em território, com uma área de 1 559 159,148 km², constituindo-se na nona maior subdivisão mundial. Seria o décimo sexto<br> maior país do mundo em área territorial.",
+		clima: "O Amazonas é dominado pelo clima equatorial.",
+		pturistico: "Novo Airão, Barcelos, Parque Nacional de Anavilhanas, Itaocatiara...",
+		cultura: "A cultura amazonense tem seu referencial em três raízes étnicas bem distintas entre si, sendo essencialmente de base ameríndia e europeia. Há registros de diversos festivais nos municípios do estado.<br> Muitos dos festivais mostram a influência do folclore da região. O Carnaval de Manaus é realizado todos os anos na cidade, entre os principais eventos estão o desfile de escolas de samba, o tradicional desfile de fantasias (que acontece no Teatro Amazonas, próximo ao Centro Histórico de Manaus), além do Carnaboi."
+	},
+			"minas" : {
+		imagem:	"Imagens/minas.jpg",
+		titulo: "Minas Gerais",
+		desc: "Minas Gerais é uma das 27 unidades federativas do Brasil, sendo o quarto estado com a maior área territorial e o segundo em quantidade de habitantes, localizada na Região Sudeste do país",
+		clima: "No estado de Minas Gerais, predominam quatro tipos distintos de clima: o clima subtropical de altitude, o clima subtropical de inverno seco, clima tropical com inverno seco e e o clima seco com chuvas no verão.",
+		pturistico: "Mineirão, Lagoa da Pumpulha, Igreja de São Francisco de Assis, Parque do Sabiá...",
+		cultura: "Minas Gerais conta com vários espaços, projetos e eventos dedicados ao fomento das áreas teatral e de dança. O estado é berço de uma considerável gama de grupos teatrais de sucesso nacional ou mesmo internacional."
+	},
+	"paris" : {
+		imagem:	"Imagens/paris.jpg",
+		titulo: "Paris",
+		desc: "Paris é a capital mais populosa cidade da França.",
+		clima: "De tipo oceânico de transição",
+		pturistico: "Torre Eiffel,Arco do Triunfo,Museu do Louvre,Panteão...",
+		cultura: "Paris tem um lugar privilegiado no âmbito artístico e cultural a nível mundial nos últimos séculos.<br> Nasceram na cidade movimentos artísticos como o expressionismo, o surrealismo e o fauvismo e importantes figuras da arte e pensamento como René Descartes, Voltaire, Victor Hugo, Émile Zola..."
+	},
+		"dubai" : {
+		imagem:	"Imagens/dubai.jpg",
+		titulo: "Dubai",
+		desc: "Dubai é a maior cidade e emirado de mesmo nome dos Emirados Árabes Unidos.",
+		clima: "Predominantemente quente e árido",
+		pturistico: "Palm Islands,The World,Burj Al Arab,Burj Khalifa...",
+		cultura: "Dubai é famosa por suas obras grandiosas e de forte apelo turístico. Dentre elas, podemos destacar as Palm Islands, o arquipélago The World, o hotel Burj Al Arab e o edifício Burj Khalifa."
+	},
+			"newyork" : {
+		imagem:	"Imagens/ny.jpg",
+		titulo: "New York",
+		desc: "New York é um estado no nordeste dos Estados Unidos e é o 27º mais extenso.",
+		clima: "Clima continental úmido",
+		pturistico: "Central Park,Estátua da Liberdade,Empire State Building,Times Square...",
+		cultura: "É uma cidade altamente ativa. Possui muitos cognomes: O mais famoso deles é (A Grande Maçã)."
+	},
+			"puntacana" : {
+		imagem:	"Imagens/puntacana.jpeg",
+		titulo: "Punta Cana",
+		desc: "Punta Cana faz parte do distrito municipal de Punta Cana-Bávaro-Verón-Macao,em La Altagracia,a província mais oriental <br>da República Dominicana.",
+		clima: "O tempo é bastante consistente ao longo do ano, com uma temperatura média de 30°C.",
+		pturistico: "Cabo Engaño,Cap Cana,Altos de Chavón...",
+		cultura: "É uma cultura versátil, onde possui pessoas de toda a parte do mundo, para desfrutar de suas praias paradisíacas."
+	},
+			"orlando" : {
+		imagem:	"Imagens/disney.jpg",
+		titulo: "Orlando",
+		desc: "Orlando é uma cidade localizada no estado norte-americano da Flórida,no condado de Orange.",
+		clima: "Possui um clima subtropical quente e úmido,onde duas estações climáticas são bastantes peculiares.",
+		pturistico: "Islands of Adventure,Magic kingdom,Harry P.Leu Gardens,Typhoon Lagoon...",
+		cultura: "Orlando é tão conhecida por seus parques temáticos insuperáveis que seu rico patrimônio artístico e cultural fica, muita vezes, de lado. Entretanto, a Grande Orlando ostenta alguns dos mais famosos museus e galerias do mundo. De fato, o Museu Morse de Arte Americana possui a maior coleção de obras do artista americano Louis Comfort Tiffany. Orlando tem suas próprias companhias de ópera e de balé,e teatros."
+	},
+
+     "grecia" : {
+	 imagem:	"Imagens/grecia.jpg",
+   titulo: "Grécia",
+	 desc: "Grécia,oficialmente República Helênica ou Helénica e conhecida desde a antiguidade como Hélade. O país está estrategicamente localizado no cruzamento entre a Europa, a Ásia, o Oriente Médio e a África.",
+	 clima: "O clima da Grécia pode ser classificado em três tipos (mediterrânico, a alpino e o temperado) que influenciam regiões<br> bem definidas do seu território.",
+	 pturistico: "Esparta,Atenas,Santorini...",
+	 cultura: "Os remanescentes físicos da cultura da Grécia clássica conservam-se principalmente em Atenas,Esparta,Micenas,Argos e outros sítios, enquanto as esculturas e outros objetos de arte exibidos nos museus gregos e dos principais centros culturais do mundo constituem uma lembrança permanente de copiosa herança cultural helênica, que ainda continua viva na educação dos gregos."
+  },
+
+    "italia" : {
+  imagem: "Imagens/italia.jpg",
+	titulo: "Itália",
+	desc: "Itália,oficialmente República Italiana, é uma república parlamentar unitária localizada no centro-sul da Europa.",
+	clima: "O clima da Itália varia de região para região. O norte da Itália (Milão, Turim e Bolonha) tem um clima continental, quando ao sul de Florença apresenta o clima mediterrânico, com verões tipicamente secos e ensolarados.",
+	pturistico: "Milão,Roma,Florença,Bolonha,Palermo...",
+	cultura: "A Itália é um dos países que mais influência teve e tem na cultura europeia e mundial, em todas as áreas da arte e cultura.<br>Os italianos podem vangloriar-se de uma longa tradição cultural das artes às ciências e tecnologia, e uma forte tradição de excelência em todas as artes, culturas, literatura e ciências,corroborado no facto de o país possuir o maior número de patrimônios da UNESCO, totalizando 44. São nomes italianos< grandes polímatas, artistas e gênios, como Dante, Leonardo da Vinci, Michelangelo e Enrico Fermi.",
+
+	},
+
+		"chile" : {
+  imagem: "Imagens/chile.jpg",
+	titulo: "Chile",
+	desc: "Chile,oficialmente República do Chile, é um país da América do Sul, que ocupa uma longa e estreita faixa costeira encravada entre a cordilheira dos Andes e o oceano Pacífico.",
+	clima: "O clima do Chile compreende uma ampla gama de condições climáticas através de uma larga escala geográfica, estendendo-se através de 38 graus de latitude.",
+	pturistico: "Ilha de Páscoa,Santiago,Parque Nacional Torres del Paine,Pucón,Puerto Varas...",
+	cultura: "Os chilenos chamam seu país de 'país de poetas'. O esporte mais popular do Chile é o futebol. A Gastronomia chilena é um reflexo da variedade topográfica do país, que caracteriza uma variedade de frutos do mar, carne, frutas e legumes."
+	}
+
+}
+	
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function especdetalhes(it) {
+	document.getElementById('it_imagem').src = it.imagem;
+	document.getElementById('it_titulo').innerHTML = it.titulo;
+	document.getElementById('it_desc').innerHTML = it.desc;
+	document.getElementById('it_clima').innerHTML = it.clima;
+	document.getElementById('it_pturistico').innerHTML = it.pturistico;
+	document.getElementById('it_cultura').innerHTML = it.cultura;
+}
+
+var infos = getParameterByName('infos');
+especdetalhes(espec[infos]); 
